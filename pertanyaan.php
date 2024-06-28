3417673e56370fa329c9b0895d6ad7d08bc7ba76
ToshiTaff12 <toshi.tfc777@gmail.com>
2024-06-28 09:37:36 +0700

    Mengubah style pertanyaan

--- pertanyaan.php
+++ pertanyaan.php
@@ -12,94 +12,45 @@
 
 <body>
 
-    <nav>
-        <h1>Daftar Pertanyaan</h1>
-        <div class="tod">
-            <a href="#">Truth</a>
-            <a href="dare.php">Dare</a>
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Daftar Pertanyaan</a>
+            <div class="tod">
+                <a href="#" class="btn btn-light">Truth</a>
+                <a href="dare.php" class="btn btn-light">Dare</a>
+            </div>
         </div>
     </nav>
 
-    <div class="row">
-        <div class="card text-bg-light mb-3" style="max-width: 18rem;">
-            <div class="card-header">Truth</div>
-            <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                    card's
-                    content.</p>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-4 mb-3">
+                <div class="card text-bg-light">
+                    <div class="card-header">Truth</div>
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                            card's content.</p>
+                    </div>
+                </div>
             </div>
-        </div>
 
-        <div class="card text-bg-light mb-3" style="max-width: 18rem;">
-            <div class="card-header">Truth</div>
-            <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                    card's
-                    content.</p>
-            </div>
-        </div>
+            <div class="col-md-4 mb-3">
+                <div class="card text-bg-light">
+                    <div class="card-header">Truth</div>
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                            card's content.</p>
+                    </div>
+                </div>
 
-        <div class="card text-bg-light mb-3" style="max-width: 18rem;">
-            <div class="card-header">Truth</div>
-            <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                    card's
-                    content.</p>
-            </div>
-        </div>
-
-        <div class="card text-bg-light mb-3" style="max-width: 18rem;">
-            <div class="card-header">Truth</div>
-            <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                    card's
-                    content.</p>
-            </div>
-        </div>
-
-        <div class="card text-bg-light mb-3" style="max-width: 18rem;">
-            <div class="card-header">Truth</div>
-            <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                    card's
-                    content.</p>
-            </div>
-        </div>
-
-        <div class="card text-bg-light mb-3" style="max-width: 18rem;">
-            <div class="card-header">Truth</div>
-            <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                    card's
-                    content.</p>
-            </div>
-        </div>
-
-        <div class="card text-bg-light mb-3" style="max-width: 18rem;">
-            <div class="card-header">Truth</div>
-            <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                    card's
-                    content.</p>
-            </div>
-        </div>
-
-        <div class="card text-bg-light mb-3" style="max-width: 18rem;">
-            <div class="card-header">Truth</div>
-            <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                    card's
-                    content.</p>
-            </div>
         </div>
     </div>
 
-    <div class="next">
+    <div class="next text-end mt-4">
         <a href="spin wheel.php" class="btn btn-primary">
-            <p>NEXT</p>
+            NEXT
         </a>
     </div>
-    
-</body>
 
+</body>
 </html>
