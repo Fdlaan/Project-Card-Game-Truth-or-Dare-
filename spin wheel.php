2388df67533d1eba63e9fb86894fbca339faeb5f
Fdlaan <fadlanadli30@gmail.com>
2024-07-02 13:46:07 +0700

    Update spin wheel.php

--- spin wheel.php
+++ spin wheel.php
@@ -86,6 +86,7 @@ $image = isset($_SESSION['background_image']) ? $_SESSION['background_image'] : 
       border-radius: 10px;
       text-align: center;
       margin-left: 35px;
+      margin-block: 35%;
       height: fit-content;
     }
 
