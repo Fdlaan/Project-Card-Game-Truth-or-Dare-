632938b831dc67eefbc89346dc64db4548bb35e9
ToshiTaff12 <toshi.tfc777@gmail.com>
2024-07-08 09:41:16 +0700

    Menambahkan pop up tutorial di page title,php

--- title.php
+++ title.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Title</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"></head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
+</head>
 <body>
     
     <img src="assets/title.png" alt="gambar title">
@@ -15,5 +16,36 @@
             <button onclick="window.location.href = 'kartu.php';" type="button" class="btn btn-primary btn-lg"><p>START GAME</p></button>
         </form>
     </div>
+
+    <!-- POP UP Tutorial -->
+    <div class="modal fade" id="tutorialModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Tutorial</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <video class="embed-responsive-item w-100" controls>
+                            <source src="assets/WhatsApp Video 2024-03-12 at 18.31.17.mp4" type="video/mp4">
+                        </video>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Skip</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
+    <script>
+        window.onload = function() {
+            var myModal = new bootstrap.Modal(document.getElementById('tutorialModal'), {});
+            myModal.show();
+        };
+    </script>
 </body>
 </html>
