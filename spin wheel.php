a31fb102aee3f7f1d744e49b82b3b2de329893e2
ToshiTaff12 <toshi.tfc777@gmail.com>
2024-07-15 18:25:22 +0700

    Mengganti icon

--- spin wheel.php
+++ spin wheel.php
@@ -39,6 +39,7 @@ $conn->close();
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Spinning Wheel</title>
+  <link rel="icon" href="assets/TOD-removebg-preview.png">
   <style>
     body {
       display: flex;
